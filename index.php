a57f0e6ee86bd9d93dee36808025eb086eeb7940
indrafarros <indrafarros.fi@gmail.com>
2021-03-02 13:29:12 +0700

    proses

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indra Mochamad Farros</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script/custom.js"></script>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset='70'>
+
+    <header>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-sm bg-white fixed-top">
+            <h1>
+                <a href="" class="navbar-brand ml-4 logo">Indra.</a>
+            </h1>
+            <!-- Button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseibleNavbar">
+                <i class="bx bx-menu"></i>
+                <i class="bx bx-x" style="display:none"></i>
+
+            </button>
+
+            <!-- nav -->
+            <div class="collapse navbar-collapse justify-content-end" id="collapseibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item py-3 py-md-0 mx-sm-2 mx-md-3 mx-lg-5"><a href="#home" class="nav-link">Home</a></li>
+                    <li class="nav-item py-3 py-md-0 mx-sm-2 mx-md-3 mx-lg-5"><a href="#about" class="nav-link">About</a></li>
+                    <li class="nav-item py-3 py-md-0 mx-sm-2 mx-md-3 mx-lg-5"><a href="#skills" class="nav-link">Skills</a></li>
+                    <li class="nav-item py-3 py-md-0 mx-sm-2 mx-md-3 mx-lg-5"><a href="#work" class="nav-link">Portofolio</a></li>
+                    <li class="nav-item py-3 py-md-0 mx-sm-2 mx-md-3 mx-lg-5"><a href="#contact" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Section -->
+    <section id="home">
+        <div class="container my-4 py-4 my-md-4 py-md-5">
+            <div class="row my-4 py-4 my-md-4 py-md-5">
+                <div class="social-media col-2 col-md-2 pt-4 mt-3 pt-md-0 mt-md-0">
+                    <li>
+                        <i class='bx bxl-instagram'></i>
+                    </li>
+                    <li class="py-4">
+                        <i class='bx bxl-github'></i>
+                    </li>
+                    <li class="py-4">
+                        <i class='bx bxl-dribbble'></i>
+                    </li>
+                </div>
+
+                <div class="col-10 col-sm-10 col-md-4 pt-5">
+                    <h3>Hi,</h3>
+                    <h1>I am <span class="theme-text">Indra Mochamad Farros</span></h1>
+                    <h4>Web Developer</h4>
+                    <button class="btn btn-lg btn-dark mt-3 px-4">Hire</button>
+                </div>
+                <div class="col-sm-12 col-md-6" id="person-img"></div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="my-md-4 py-md-3">
+        <div class="container my-md-4 py-md-3">
+            <div class="row">
+                <div class="title col-12 col-md-4">
+                    <h1 class="pb-4 py-md-0">Ab<span class="theme-text">out.</span></h1>
+                </div>
+                <div class="col-12 col-md-8 col-lg-8">
+                    <h4>Indra Mochamad Farros <small> <i>Web Developer</i></small></h4>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem totam cumque voluptatibus quod nulla. Dignissimos, nesciunt! Eligendi mollitia ex consectetur blanditiis rem, ratione, tempore quo dicta, sunt hic minus minima!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem totam cumque voluptatibus quod nulla. Dignissimos, nesciunt! Eligendi mollitia ex consectetur blanditiis rem, ratione, tempore quo dicta, sunt hic minus minima!</p>
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="skills">
+        <div class="container my-md-4 py-md-3">
+            <div class="row">
+                <div class="col-12 col-lg-8 col-md-8">
+                    <h4>Skills <small><i>My knowledge</i></small></h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente ratione molestiae, explicabo magni nisi labore amet ex reiciendis velit. Perspiciatis, provident eius! Culpa perspiciatis, ad recusandae sequi nulla voluptas cumque!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente ratione molestiae, explicabo magni nisi labore amet ex reiciendis velit. Perspiciatis, provident eius! Culpa perspiciatis, ad recusandae sequi nulla voluptas cumque!</p>
+                </div>
+                <div class="title col-12 col-sm-12 col-md-4">
+                    <h1 class="py-4 py-md-0">Sk<span class="theme-text">ills.</span></h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum facere vero sequi accusamus accusantium odio enim esse pariatur! Incidunt autem molestiae itaque a nisi labore consequatur harum facere doloremque non.</p>
+                </div>
+            </div>
+            <div class="row my-5 py-3 border">
+                <div class="col-lg-12">
+                    <div class="row text-center">
+                        <div class="col-lg-6">
+                            <h3 class="py-3">Web Development</h3>
+                            <div class="text-left">
+                                <div class="progress-con py-3 my-3 p-3">
+                                    <h5>HTML</h5>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: 90%"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-con py-3 my-3 p-3">
+                                    <h5>HTML</h5>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: 90%"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-con py-3 my-3 p-3">
+                                    <h5>HTML</h5>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: 90%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3 class="py-3">UI/UX</h3>
+                            <div class="text-left">
+                                <div class="progress-con my-3 p-3">
+                                    <h5>Adobe XD</h5>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width:50%"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-con my-3 p-3">
+                                    <h5>Adobe XD</h5>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width:50%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="work" class="my-md-3 py-md-3">
+        <div class="container" class="my-md-3 py-md-3">
+            <div class="row">
+                <div class="title col-12 col-md-4">
+                    <h1 class="pb-5 pb-md-0">Wo<span class="theme-text">rks.</span></h1>
+                </div>
+                <div class="col-12 col-md-8 col-lg-8">
+                    <h4>Work<small><i>that i work</i></small></h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis suscipit quisquam repellendus asperiores, numquam labore recusandae error repellat alias nihil corrupti tempora nulla quae corporis exercitationem adipisci aspernatur cupiditate nostrum.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis suscipit quisquam repellendus asperiores, numquam labore recusandae error repellat alias nihil corrupti tempora nulla quae corporis exercitationem adipisci aspernatur cupiditate nostrum.</p>
+                </div>
+            </div>
+            <div class="container my-5 my-md-3 py-3 py-md-0 work-info">
+                <nav class="navbar navbar-expand justify-content-center">
+                    <ul class="navbar-nav" id="work-nav">
+                        <li class="nav-item mx-sm-3 mx-md-5">
+                            <p class="nav-link all" id="active" onclick="display('all')">All</p>
+                        </li>
+                        <li class="nav-item mx-sm-3 mx-md-5">
+                            <p class="nav-link api" onclick="display('api')">Api</p>
+                        </li>
+                        <li class="nav-item mx-sm-3 mx-md-5">
+                            <p class="nav-link all" onclick="display('web')">Web</p>
+                        </li>
+                        <li class="nav-item mx-sm-3 mx-md-5">
+                            <p class="nav-link all" onclick="display('game')">Game</p>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="row justify-content-center" id="work-container">
+                    <div id="api" class="col-12 col-md-8 col-lg-5 p-5 m-4 border">
+                        <h4 class="text-center">Web Development</h4>
+                        <div class="text-center">
+                            <i class="bx bx-fingerprint p-3"></i>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum consequatur repellendus tempore tenetur debitis rem, explicabo suscipit soluta quia quaerat? Nesciunt, quo sed atque ducimus minima similique voluptatibus velit.</p>
+                    </div>
+                    <div id="game" class="col-12 col-md-8 col-lg-5 p-5 m-4 border">
+                        <h4 class="text-center">Web Development</h4>
+                        <div class="text-center">
+                            <i class="bx bx-fingerprint p-3"></i>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum consequatur repellendus tempore tenetur debitis rem, explicabo suscipit soluta quia quaerat? Nesciunt, quo sed atque ducimus minima similique voluptatibus velit.</p>
+                        <div class="btn btn-lg btn-dark mt-3 px-4">View</div>
+                    </div>
+                    <div id="web" class="col-12 col-md-8 col-lg-5 p-5 m-4 border">
+                        <h4 class="text-center">Web Development</h4>
+                        <div class="text-center">
+                            <i class="bx bx-fingerprint p-3"></i>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum consequatur repellendus tempore tenetur debitis rem, explicabo suscipit soluta quia quaerat? Nesciunt, quo sed atque ducimus minima similique voluptatibus velit.</p>
+                        <div class="btn btn-lg btn-dark mt-3 px-4">View</div>
+                    </div>
+                    <div id="api" class="col-12 col-md-8 col-lg-5 p-5 m-4 border">
+                        <h4 class="text-center">Web Development</h4>
+                        <div class="text-center">
+                            <i class="bx bx-fingerprint p-3"></i>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum consequatur repellendus tempore tenetur debitis rem, explicabo suscipit soluta quia quaerat? Nesciunt, quo sed atque ducimus minima similique voluptatibus velit.</p>
+                        <div class="btn btn-lg btn-dark mt-3 px-4">View</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact" class="mt-4 pt-5 mt-md-2 pt-md-2">
+        <div class="title col-12 col-md-12">
+            <h1 class="pb-3">Con<span class="theme-text">tacts.</span></h1>
+            <div class="container-fluid jumbotron mb-0 mt-4 pt-5">
+                <div class="col-12 col-md-12 col-md-8 col-lg-8 mx-auto">
+                    <form action="" class="col-12 col-sm-12 col-md-8 mx-auto">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" placeholder="Enter your name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" class="form-control" placeholder="Enter your name">
+                        </div>
+                        <div class="form-group">
+                            <label for="msg">Message</label>
+                            <textarea name="msg" class="form-control" id="msg"></textarea>
+                        </div>
+                        <div class="btn btn-dark">Submit</div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container-fluid bg-dark py-3">
+            <div class="row">
+                <div class="foot-icon d-flex flex-row justify-content-center col-12 py-4">
+                    <li class="mx-3"><i class='bx bxl-github'></i></li>
+                    <li class="mx-3"><i class='bx bxl-linkedin-square' style='color:#ffffff'></i></li>
+                    <li class="mx-3"><i class='bx bxl-whatsapp' style='color:#ffffff'></i></li>
+                </div>
+                <div class="copyright col-12 d-flex justify-content-center">
+                    <h4>2021 @ Indra Mochamad Farros</h4>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+    <!-- Box Icon -->
+    <!-- <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script> -->
+
+    <!-- Custom JS -->
+    <script src="script/custom.js"></script>
+</body>
+
+
+</html>
---
+++ script/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.bx-menu').toggle('fast');
+        $('.bx-x').toggle('fast');
+    });
+
+    $('p.nav-link').click(function(){
+        $('p.nav-link').removeAttr('Id');
+        $('p.nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+function display(elem){
+    var org="#"+elem;
+    switch(org){
+        case '#all':
+            $('#work-container div').removeClass('hide-element');
+            break;
+        case '#web':
+            $('#work-container #web').removeClass('hide-element');
+            $('#work-container #game').addClass('hide-element');
+            $('#work-container #api').addClass('hide-element');
+            break; 
+        case '#api':
+            $('#work-container #api').removeClass('hide-element');
+            $('#work-container #game').addClass('hide-element');
+            $('#work-container #web').addClass('hide-element');
+            break; 
+        
+    }
+}
